dd6b3e0d1e15d7e7747dd5f44638173a84f108c5
WooJin Kim <dnwls813@ajou.ac.kr>
2022-09-14 04:23:17 +0900

    Add: One-Time Chat Room

---
+++ backend/chat/src/main/java/ari/chat/domain/ChatMessage.java
@@ -0,0 +1,30 @@
+package ari.chat.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    public enum MessageType {
+        ENTER, TALK
+    }
+
+    private MessageType type;
+    //채팅방 ID
+    private String roomId;
+    //보내는 사람
+    private String sender;
+    //내용
+    private String message;
+}
