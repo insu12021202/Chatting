785c089753bd933c8613a915754892cdf83c2512
WooJin Kim <dnwls813@ajou.ac.kr>
2022-09-19 17:23:49 +0900

    Update: 메세지를 db에  저장하도록 변경

--- backend/chat/src/main/java/ari/chat/controller/MessageController.java
+++ backend/chat/src/main/java/ari/chat/controller/MessageController.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,11 +21,12 @@ public class MessageController {
     // 메세지의 destination이 /chat/message이면 해당 sendMessage() method가 불리도록 해줌
     @MessageMapping("/chat/sendMessage") // 클라이언트에서 /app/chat/sendMessage로 요청되는 메시지
     //@SendTo("/topic/public")
-    public void enter(@Payload ChatMessage message) {
-       // if (ChatMessage.MessageType.JOIN.equals(message.getType())) {
-       //     message.setContent(message.getSender()+"님이 입장하였습니다.");
-       // }
+    public void enter(@Payload ChatMessage message, @RequestParam Long roomId, @RequestParam String sender) {
+        if (ChatMessage.MessageType.JOIN.equals(message.getType())) {
+            message.setContent(message.getSender()+"님이 입장하였습니다.");
+       }
 
+        chatService.saveMessage(message, roomId, sender);
 
         sendingOperations.convertAndSend("/topic/public",message);
     }
