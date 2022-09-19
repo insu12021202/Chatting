67df711a52a481f10d59d179db776d32cbc4f0f4
YeoInsu <insu12021202@gmail.com>
2022-09-19 17:56:53 +0900

    Modify:App JS

--- frontend/chatting/src/App.js
+++ frontend/chatting/src/App.js
@@ -1,10 +1,68 @@
+import { Stomp } from "@stomp/stompjs";
+import axios from "axios";
 import { useEffect, useState } from "react";
+import SockJS from "sockjs-client";
 import "./App.css";
 
 function App() {
   const [username, setUsername] = useState("");
+  const [roomID, setRoomID] = useState();
+  const [roomName, setRoomName] = useState("");
+  let stompClient = null;
+
+  //username과 roomName을 보내주고 roomID와 roomName 받아오기
+  const getRoomData = async () => {
+    // await axios.get("/hello").then((res) => {
+    //   console.log(res.data);
+    // });
+    await axios
+      .post("/chat/room", { name: "roomName", user1: username })
+      .then((res) => {
+        setRoomID(res.data.roomID);
+        setRoomName(res.data.roomName);
+        console.log("aa", res.data);
+      });
+  };
+  //connet 함수
+  const connect = () => {
+    if (username) {
+      var socket = new SockJS("/ws");
+      stompClient = Stomp.over(socket);
+
+      stompClient.connect({}, onConnected, () => {
+        console.log("Error");
+      });
+    }
+  };
+
+  const onConnected = async () => {
+    await getRoomData();
+    if (roomID) {
+      stompClient.subscribe(`/queue/room/${roomID}`, onMessageReceived);
+
+      // Tell your username to the server
+      stompClient.send(
+        "/app/chat/addUser",
+        {},
+        JSON.stringify({ sender: username, type: "JOIN" })
+      );
+    }
+  };
+
+  const onMessageReceived = (payload) => {
+    var message = JSON.parse(payload.body);
+
+    if (message.type === "JOIN") {
+      console.log("Join " + message.sender);
+    } else if (message.type === "LEAVE") {
+      console.log("Leave ", message.sender);
+    } else {
+      console.log("Chatting: ", message.sender);
+    }
+  };
+  //버튼 클릭 시
   const onClick = () => {
-    console.log(username);
+    connect();
   };
   const onChange = (e) => {
     setUsername(e.target.value);
